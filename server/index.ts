02a6c3ec22d4e1e87771fc901fddb9bf01614baa
Tetsuya Katsuragi <webmaster.smashingpumpkins@gmail.com>
2026-07-20 12:55:45 +0900

    fix: Bind Express server explicitly to 0.0.0.0 for external network access

--- server/index.ts
+++ server/index.ts
@@ -117,6 +117,6 @@ app.get('*', (_req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(Number(PORT), '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
